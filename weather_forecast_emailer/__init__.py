a85fe4cc121cf4ec546c4bdcf5377c58023b03be
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-11-25 02:15:30 +0000

    Add a forecast class to connect to OpenWeatherMap API

--- weather_forecast_emailer/__init__.py
+++ weather_forecast_emailer/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
     'constants'
     'email_list_reader',
+    'forecast'
     'forecast_response',
     'forecast_response_error',
     'forecast_response_formatter',
@@ -9,6 +10,7 @@ __all__ = [
 
 from .constants import *
 from .email_list_reader import EmailListReader
+from .forecast import Forecast
 from .forecast_response import ForecastResponse
 from .forecast_response_error import ForecastResponseError
 from .forecast_response_formatter import ForecastResponseFormatter
