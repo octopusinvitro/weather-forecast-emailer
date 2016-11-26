9e49c5a347022acb49362cc9f7dd8ac54b8b8f5a
private <user@example.com>
2016-11-26 00:15:07 +0000

    Add a wrapper for the forecast response received from the API

--- weather_forecast_emailer/__init__.py
+++ weather_forecast_emailer/__init__.py
@@ -1,7 +1,11 @@
 __all__ = [
+    'constants'
     'email_list_reader',
+    'forecast_response',
     'printer',
 ]
 
+from .constants import *
 from .email_list_reader import EmailListReader
+from .forecast_response import ForecastResponse
 from .printer import Printer
